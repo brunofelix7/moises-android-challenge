faccfcbc1517d6e9ea994ebbe14831df0054d9ab
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-22 15:01:22 -0300

    ➕ chore(deps): add lifecycle-runtime-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
 
     // Lifecycle
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 
