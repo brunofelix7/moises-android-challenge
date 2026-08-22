a508819682a6d66af9ccb163cdfbfe61f493eb13
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-22 10:54:57 -0300

    ⬆️ chore(build): bump compileSdk version to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "dev.brunofelix.moiseschallenge"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
