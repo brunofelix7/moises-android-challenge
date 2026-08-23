86c0cb9e5bd6594792f65b17f84887fdf2afc041
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-23 15:36:28 -0300

    ✨ feat(data): add SongRemoteDataSource interface

---
+++ app/src/main/java/dev/brunofelix/moiseschallenge/core/data/remote/source/SongRemoteDataSource.kt
@@ -0,0 +1,27 @@
+package dev.brunofelix.moiseschallenge.core.data.remote.source
+
+import dev.brunofelix.moiseschallenge.core.domain.model.Album
+import dev.brunofelix.moiseschallenge.core.domain.model.Song
+
+/**
+ * Interface for remote data source operations related to songs.
+ */
+interface SongRemoteDataSource {
+    /**
+     * Searches for songs based on a term entered by the user.
+     *
+     * @param term The search term.
+     * @param limit The maximum number of results to return.
+     * @param offset The index of the first result to return.
+     * @return A [Result] containing the search results.
+     */
+    suspend fun search(term: String, limit: Int, offset: Int): Result<List<Song>>
+
+    /**
+     * Retrieves the details of an album and its respective tracks using the ID.
+     *
+     * @param id The ID of the song to lookup.
+     * @return A [Result] containing the lookup results.
+     */
+    suspend fun getAlbumDetails(id: Long): Result<Album>
+}
