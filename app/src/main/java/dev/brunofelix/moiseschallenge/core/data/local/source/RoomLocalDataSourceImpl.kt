2f0c83f174362dba6bf63e58f02e1a364078c356
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-23 19:55:39 -0300

    ✨ feat(data): implement RoomLocalDataSourceImpl

---
+++ app/src/main/java/dev/brunofelix/moiseschallenge/core/data/local/source/RoomLocalDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package dev.brunofelix.moiseschallenge.core.data.local.source
+
+import dev.brunofelix.moiseschallenge.core.data.local.dao.SongDao
+import dev.brunofelix.moiseschallenge.core.data.local.mapper.toDomain
+import dev.brunofelix.moiseschallenge.core.data.local.mapper.toEntity
+import dev.brunofelix.moiseschallenge.core.domain.model.Song
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class RoomLocalDataSourceImpl @Inject constructor(
+    private val dao: SongDao
+) : SongLocalDataSource {
+
+    override fun getRecentSongs(): Flow<List<Song>> {
+        return dao.getRecentSongs().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun saveRecentSong(song: Song): Long {
+        return dao.insert(song.toEntity())
+    }
+}
