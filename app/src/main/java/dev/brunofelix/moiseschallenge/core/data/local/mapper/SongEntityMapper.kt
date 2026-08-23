56a8c9b76fc804411643cdc9145a5c1f9da3435e
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-23 19:53:41 -0300

    ✨ feat(data): add SongEntityMapper for Entity to domain conversion

---
+++ app/src/main/java/dev/brunofelix/moiseschallenge/core/data/local/mapper/SongEntityMapper.kt
@@ -0,0 +1,29 @@
+package dev.brunofelix.moiseschallenge.core.data.local.mapper
+
+import dev.brunofelix.moiseschallenge.core.data.local.entity.SongEntity
+import dev.brunofelix.moiseschallenge.core.domain.model.Song
+
+fun SongEntity.toDomain(): Song {
+    return Song(
+        id = id,
+        title = title,
+        artist = artistName,
+        coverUrl = coverUrl,
+        audioUrl = audioUrl,
+        durationMillis = durationMillis,
+        albumId = albumId
+    )
+}
+
+fun Song.toEntity(): SongEntity {
+    return SongEntity(
+        id = id,
+        title = title,
+        artistName = artist,
+        coverUrl = coverUrl,
+        audioUrl = audioUrl,
+        durationMillis = durationMillis,
+        albumId = albumId ?: 0L,
+        playedAt = System.currentTimeMillis()
+    )
+}
