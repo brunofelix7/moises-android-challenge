4025db58517edb32ca0af909b3f28f1b5f154d38
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-24 23:03:04 -0300

    💄 refactor(song): add SongRoute to SongNavEntry

--- app/src/main/java/dev/brunofelix/moiseschallenge/feature/song/presentation/SongNavEntry.kt
+++ app/src/main/java/dev/brunofelix/moiseschallenge/feature/song/presentation/SongNavEntry.kt
@@ -1,15 +1,15 @@
 package dev.brunofelix.moiseschallenge.feature.song.presentation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import dev.brunofelix.moiseschallenge.core.presentation.navigation.Route
 
 fun EntryProviderScope<NavKey>.songNavEntry(
-    onNavigate: (Route) -> Unit,
-    paddingValues: PaddingValues
+    onNavigate: (Route) -> Unit
 ) {
     entry<Route.Songs> {
-        // SongRoute
+        SongRoute(
+            onNavigate = onNavigate
+        )
     }
 }
