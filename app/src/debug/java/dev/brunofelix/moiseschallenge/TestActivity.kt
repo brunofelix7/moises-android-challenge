bb0838e8bd190ddd60a6ea29a6e3fc7d153b8863
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-27 16:55:44 -0300

    ✅ test(ui): setup automated tests
    - add TestActivity on 'debug' package
    - add AndroidManifest.xml on 'debug' package

---
+++ app/src/debug/java/dev/brunofelix/moiseschallenge/TestActivity.kt
@@ -0,0 +1,7 @@
+package dev.brunofelix.moiseschallenge
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TestActivity : ComponentActivity()
